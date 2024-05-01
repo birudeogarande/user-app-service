5782412eb3a806c3ae6e567d30416037feb6a944
birudeogarande <birudeogarande@localhost>
2024-05-01 12:57:46 +0530

    added users get api

--- src/main/java/com/user/UserAppServiceApplication.java
+++ src/main/java/com/user/UserAppServiceApplication.java
@@ -1,5 +1,8 @@
 package com.user;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +20,24 @@ public class UserAppServiceApplication {
 	public String sayHello() {
 		return "Welcome to kubernetes!!";
 	}
+	
+	@GetMapping("/users")
+	public List<User> getUsers(){
+		
+		return Arrays.asList(
+								new User(1,  "Birudeo"),
+								new User(2,  "Abinash"),
+								new User(3,  "Ajay"),
+								new User(4,  "Saatvik"),
+								new User(5,  "Dipali"),
+								new User(6,  "Suresh"),
+								new User(7,  "Suraj")
+				);
+	}
+	
+	
 
 }
+
+
+record User(Integer id, String name) {}
